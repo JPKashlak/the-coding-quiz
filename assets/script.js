be00a303d47e418aaa40dc3ab64950c28bf34acb
jpkashlak <jpkashlak@gmail.com>
2021-01-03 14:56:08 -0600

    added removeStart function to beginQuiz

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,33 @@
 var quizContentEl = document.querySelector("#quiz");
 
 // Questions
-var questionArray = [{
-    number: 1,
-    text: "What event listener did I just use to start this quiz?",
-    answers: ["Click", "Drag", "Hover", "Sing"],
-    solution: [true, false, false, false],
-},
+var questionArray = [
+    "What event listener did I just use to start this quiz?",
+    "Why?",
+    "How?",
+    "q4",
+    "q5",
+    "q6",
+    "q7",
+    "q8",
+    "q9",
+    "q10"
+    // answers: ["Click", "Drag", "Hover", "Sing"],
+    // solution: [true, false, false, false],
 
 ];
 
 
 var beginQuiz = function(event) {
     console.log(event);
-    var listQuestionEl = document.createElement("h2");
+
+    // Deletes Start Button At Quiz Start
+    var removeStart = document.getElementById("startButton");
+        removeStart.remove();
+
+    document.getElementById("questionSlot").innerHTML = questionArray[0];
+    
+
 
 };
 
